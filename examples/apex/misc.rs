b46ac8cb67ac5d3f4e8d8a6f479a7e07e5981e8b
Casper <CasualX@users.noreply.github.com>
2019-06-08 20:56:57 +0200

    Update Apex Legends example

--- examples/apex/misc.rs
+++ examples/apex/misc.rs
@@ -4,11 +4,18 @@ use pelite::pattern as pat;
 
 pub fn print(bin: PeFile<'_>, dll_name: &str) {
 	println!("## Miscellaneous\n\n```");
+	header(bin);
 	entity_list(bin, dll_name);
 	local_entity_handle(bin, dll_name);
 	global_vars(bin, dll_name);
 	player_resource(bin, dll_name);
 	println!("```\n");
+}
+
+fn header(bin: PeFile<'_>) {
+	// Check if offsets are correct
+	println!("TimeDateStamp = {:#x}", bin.file_header().TimeDateStamp);
+	println!("CheckSum = {:#x}", bin.optional_header().CheckSum);
 }
 
 fn entity_list(bin: PeFile<'_>, dll_name: &str) {
@@ -24,7 +31,7 @@ fn entity_list(bin: PeFile<'_>, dll_name: &str) {
 		let num_ent_entries = save[1];
 		let cl_entitylist = save[2];
 		println!("NUM_ENT_ENTRIES = {:#x}", num_ent_entries);
-		println!("{}!{:#010x} cl_entitylist", dll_name, cl_entitylist);
+		println!("{}!{:#x} cl_entitylist", dll_name, cl_entitylist);
 	}
 	else {
 		eprintln!("unable to find cl_entitylist!");
@@ -33,9 +40,9 @@ fn entity_list(bin: PeFile<'_>, dll_name: &str) {
 
 fn local_entity_handle(bin: PeFile<'_>, dll_name: &str) {
 	let mut save = [0; 4];
-	if bin.scanner().finds_code(pat!("488D15???? 833D${?'}FF 741F"), &mut save) {
+	if bin.scanner().finds_code(pat!("833D${?'}FF 74? 0FB70D${'} 0FB705${'}"), &mut save) {
 		let local_entity_handle = save[1];
-		println!("{}!{:#010x} LocalEntityHandle", dll_name, local_entity_handle);
+		println!("{}!{:#x} LocalEntityHandle", dll_name, local_entity_handle);
 	}
 	else {
 		eprintln!("unable to find LocalEntityHandle!");
@@ -46,9 +53,9 @@ fn global_vars(bin: PeFile<'_>, dll_name: &str) {
 	// Right above "Client.dll Init_PostVideo() in library "
 	// lea r8, qword_XXX
 	let mut save = [0; 4];
-	if bin.scanner().finds_code(pat!("488B01 4C8D05${'} 498BD6 FF5020"), &mut save) {
+	if bin.scanner().finds_code(pat!("488B01 4C8D05${'} [17] $\"Client.dll Init_PostVideo\""), &mut save) {
 		let global_vars = save[1];
-		println!("{}!{:#010x} GlobalVars", dll_name, global_vars);
+		println!("{}!{:#x} GlobalVars", dll_name, global_vars);
 	}
 	else {
 		eprintln!("unable to find GlobalVars!");
@@ -59,9 +66,9 @@ fn player_resource(bin: PeFile<'_>, dll_name: &str) {
 	// References "#UNCONNECTED_PLAYER_NAME" and the C_PlayerResource vtable
 	// At the very end of the constructor assigns this to global variable
 	let mut save = [0; 4];
-	if bin.scanner().finds_code(pat!("48 8B 6C 24 60  48 8B C3  48 89 1D $'"), &mut save) {
+	if bin.scanner().finds_code(pat!("488B6C24? 488BC3 48891D$'"), &mut save) {
 		let player_resource = save[1];
-		println!("{}!{:#010x} PlayerResource", dll_name, player_resource);
+		println!("{}!{:#x} PlayerResource", dll_name, player_resource);
 	}
 	else {
 		eprintln!("unable to find PlayerResource!");
