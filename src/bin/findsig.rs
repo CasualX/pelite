b2f876557539991283b0113d88997e7e6f4952d8
Casper <CasualX@users.noreply.github.com>
2020-04-22 17:19:31 +0200

    Add JSON output to findsig utility (#230)

--- src/bin/findsig.rs
+++ src/bin/findsig.rs
@@ -5,6 +5,7 @@ Find patterns utility.
 use std::{cmp, env};
 use std::path::Path;
 use std::io::{self, Write};
+use std::ffi::OsStr;
 
 use pelite::pattern as pat;
 
@@ -25,40 +26,77 @@ fn main() {
 	let mut args = env::args_os();
 	args.next();
 
-	// Read the input file
-	if let Some(file_path) = args.next() {
-		// Path and filename of the input file
-		let file_path = Path::new(&file_path);
-		let file_name = file_path.file_name().expect("path does not have a file name").to_str().unwrap();
-
-		// Map the file into memory
-		let file_map = pelite::FileMap::open(&file_path).expect("cannot open the input file");
-
-		match pelite::PeFile::from_bytes(&file_map) {
-			Ok(file) => {
-				process_patterns(args, &mut |pattern, save| {
-					let mut matches = file.scanner().matches(pattern, file.headers().image_range());
-					while matches.next(save) {
-						print_match(file_name, save);
-					}
-				});
-			}
-			// Must be a valid PE file
-			Err(err) => {
-				panic!("Not a valid PE file: {}", err);
+	// Read the input file and/or get json option
+	let mut file_path = match args.next() {
+		Some(file_path) => file_path,
+		None => {
+			print!("{}", HELP_TEXT);
+			return;
+		},
+	};
+	let mut json = false;
+	if file_path == OsStr::new("-json") {
+		json = true;
+		file_path = match args.next() {
+			Some(file_path) => file_path,
+			None => {
+				eprintln!("Missing required file path argument.");
+				return;
 			},
 		}
 	}
-	else {
-		print!("{}", HELP_TEXT);
+
+	// Get the patterns
+	let patterns: Vec<String> = args.filter_map(|arg| arg.into_string().ok()).collect();
+	if patterns.len() == 0 {
+		json = false;
+	}
+
+	// Path and filename of the input file
+	let file_path = Path::new(&file_path);
+	let file_name = file_path.file_name().expect("path does not have a file name").to_str().unwrap();
+
+	// Map the file into memory
+	let file_map = pelite::FileMap::open(&file_path).expect("cannot open the input file");
+
+	match pelite::PeFile::from_bytes(&file_map) {
+		Ok(file) => {
+			process_patterns(&patterns, json, &mut |pattern, save| {
+				let mut matches = file.scanner().matches(pattern, file.headers().image_range());
+				let mut first = true;
+				while matches.next(save) {
+					if json {
+						if first { first = false; }
+						else { print!(","); }
+						print_json(save);
+					}
+					else {
+						print_match(file_name, save);
+					}
+				}
+			});
+		}
+		// Must be a valid PE file
+		Err(err) => {
+			panic!("Not a valid PE file: {}", err);
+		},
 	}
 }
 
-fn process_patterns(args: env::ArgsOs, f: &mut dyn FnMut(&[pat::Atom], &mut [u32])) {
-	if args.len() > 0 {
+fn process_patterns(patterns: &[String], json: bool, f: &mut dyn FnMut(&[pat::Atom], &mut [u32])) {
+	if patterns.len() > 0 {
 		// Read from the command line args if available
-		for pattern_string in args.filter_map(|pattern_string| pattern_string.into_string().ok()) {
-			process_pattern(&pattern_string, f);
+		if json {
+			print!("[");
+		}
+		let mut first = true;
+		for pat in patterns {
+			if first { first = false; }
+			else { print!(","); }
+			process_pattern(pat, json, f);
+		}
+		if json {
+			println!("]");
 		}
 	}
 	else {
@@ -76,12 +114,12 @@ fn process_patterns(args: env::ArgsOs, f: &mut dyn FnMut(&[pat::Atom], &mut [u32
 			// Only process non-empty lines, otherwise ask the user again
 			let line = line.trim();
 			if line.len() > 0 {
-				process_pattern(&line, f);
+				process_pattern(&line, false, f);
 			}
 		}
 	}
 }
-fn process_pattern(pattern_str: &str, f: &mut dyn FnMut(&[pat::Atom], &mut [u32])) {
+fn process_pattern(pattern_str: &str, json: bool, f: &mut dyn FnMut(&[pat::Atom], &mut [u32])) {
 	// Parse the pattern
 	let pattern = match pat::parse(pattern_str) {
 		Ok(pattern) => pattern,
@@ -91,10 +129,18 @@ fn process_pattern(pattern_str: &str, f: &mut dyn FnMut(&[pat::Atom], &mut [u32]
 		},
 	};
 	// Print the header followed by its matches
-	println!("Pattern `{}` matches:", pattern_str);
+	if json {
+		print!("{{\"pattern\":\"{}\",\"matches\":[", json_escape(pattern_str));
+	}
+	else {
+		println!("Pattern `{}` matches:", pattern_str);
+	}
 	let mut save = [0; 32];
 	let save_len = cmp::min(pat::save_len(&pattern), save.len());
 	f(&pattern, &mut save[..save_len]);
+	if json {
+		print!("]}}");
+	}
 }
 fn print_match(file_name: &str, save: &[u32]) {
 	print!("  {}!{:#010x}", file_name, save[0]);
@@ -106,4 +152,30 @@ fn print_match(file_name: &str, save: &[u32]) {
 		print!("]");
 	}
 	println!();
+}
+fn print_json(save: &[u32]) {
+	print!("[");
+	let mut first = true;
+	for value in save {
+		if first { first = false; }
+		else { print!(","); }
+		print!("{}", value);
+	}
+	print!("]");
+}
+fn json_escape(string: &str) -> String {
+	let mut escaped = String::new();
+	for chr in string.chars() {
+		match chr {
+			'\x08' => escaped.push_str("\\b"),
+			'\x0C' => escaped.push_str("\\f"),
+			'\n' => escaped.push_str("\\n"),
+			'\r' => escaped.push_str("\\r"),
+			'\t' => escaped.push_str("\\t"),
+			'\"' => escaped.push_str("\\\""),
+			'\\' => escaped.push_str("\\\\"),
+			_ => escaped.push(chr),
+		}
+	}
+	escaped
 }
